afdb92e8f8e5bc486c5e2e2c89824d73255ddeff
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-03-26 17:39:02 +0200

    added a FMX sample to show how to use new scores unit

---
+++ samples/Scores/ScoresSample.dpr
@@ -0,0 +1,15 @@
+program ScoresSample;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fMain in 'fMain.pas' {Form1},
+  Gamolf.RTL.Scores in '..\..\src\Gamolf.RTL.Scores.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ samples/Scores/fMain.pas
@@ -0,0 +1,100 @@
+unit fMain;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Memo.Types,
+  FMX.Layouts, FMX.Edit, FMX.StdCtrls, FMX.Controls.Presentation, FMX.ScrollBox,
+  FMX.Memo, Gamolf.RTL.Scores;
+
+type
+  TForm1 = class(TForm)
+    Memo1: TMemo;
+    btnAddScore: TButton;
+    Edit1: TEdit;
+    Layout1: TLayout;
+    btnLoadScore: TButton;
+    btnSaveScore: TButton;
+    procedure FormCreate(Sender: TObject);
+    procedure btnAddScoreClick(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
+    procedure btnLoadScoreClick(Sender: TObject);
+    procedure btnSaveScoreClick(Sender: TObject);
+  private
+    { Déclarations privées }
+  public
+    { Déclarations publiques }
+    ScoreList: TScoreList;
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.btnAddScoreClick(Sender: TObject);
+var
+  pseudo: string;
+  points, level: integer;
+begin
+  pseudo := Edit1.Text.Trim;
+  points := random(maxint);
+  if random(100) < 50 then
+  begin
+    if ScoreList.Add(pseudo, points, false) then
+      Memo1.Lines.Add('Add score : ' + pseudo + ' => ' + points.ToString)
+    else
+      Memo1.Lines.Add('Score not added : ' + pseudo);
+  end
+  else
+  begin
+    level := random(maxint);
+    if ScoreList.Add(pseudo, points, level, false) then
+      Memo1.Lines.Add('Add score : ' + pseudo + ' => ' + points.ToString +
+        ' (level ' + level.ToString + ')')
+    else
+      Memo1.Lines.Add('Score not added : ' + pseudo);
+  end;
+end;
+
+procedure TForm1.btnLoadScoreClick(Sender: TObject);
+var
+  score: tscore;
+begin
+  ScoreList.Load;
+  Memo1.Lines.Add('**********');
+  for score in ScoreList do
+  begin
+    Memo1.Lines.Add(score.pseudo + ' => ' + score.points.ToString + ' (level ' +
+      score.level.ToString + ')');
+  end;
+  Memo1.Lines.Add('**********');
+end;
+
+procedure TForm1.btnSaveScoreClick(Sender: TObject);
+begin
+  ScoreList.Save;
+  Memo1.Lines.Add('Score list saved in ' + ScoreList.GetScoreFileName);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  ScoreList := TScoreList.Create('FMXGameEngine', 'ScoresSample');
+  Memo1.Lines.Add('GetScoreFileName=' + ScoreList.GetScoreFileName);
+  Memo1.Lines.Add('GetOldScoreFileName=' + ScoreList.GetOldScoreFileName);
+end;
+
+procedure TForm1.FormDestroy(Sender: TObject);
+begin
+  ScoreList.Free;
+end;
+
+initialization
+
+randomize;
+
+end.
