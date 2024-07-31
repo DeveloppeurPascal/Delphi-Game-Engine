d279aedd8f384bdc8ed0c9ec1ff6c04c4703b4e8
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-31 15:59:41 +0200

    updated the FMX sample project (#149)

--- samples/HelpBar/FMXHelpBarSample/FMXHelpBarSample.dpr
+++ samples/HelpBar/FMXHelpBarSample/FMXHelpBarSample.dpr
@@ -3,11 +3,23 @@ program FMXHelpBarSample;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  fMain in 'fMain.pas' {Form2};
+  FMX.Skia,
+  fMain in 'fMain.pas' {Form2},
+  Gamolf.FMX.HelpBar in '..\..\..\src\Gamolf.FMX.HelpBar.pas',
+  Olf.Skia.SVGToBitmap in '..\..\..\lib-externes\librairies\src\Olf.Skia.SVGToBitmap.pas',
+  Gamolf.FMX.Joystick in '..\..\..\src\Gamolf.FMX.Joystick.pas',
+  Gamolf.RTL.Joystick.DirectInput.Win in '..\..\..\src\Gamolf.RTL.Joystick.DirectInput.Win.pas',
+  Gamolf.RTL.Joystick.Mac in '..\..\..\src\Gamolf.RTL.Joystick.Mac.pas',
+  Gamolf.RTL.Joystick in '..\..\..\src\Gamolf.RTL.Joystick.pas',
+  iOSapi.GameController in '..\..\..\src\iOSapi.GameController.pas',
+  Macapi.GameController in '..\..\..\src\Macapi.GameController.pas',
+  USVGKenneyInputKeys in '..\assets\KenneyInputKeys\USVGKenneyInputKeys.pas',
+  USVGKenneyInputXbox in '..\assets\KenneyInputXbox\USVGKenneyInputXbox.pas';
 
 {$R *.res}
 
 begin
+  GlobalUseSkia := True;
   Application.Initialize;
   Application.CreateForm(TForm2, Form2);
   Application.Run;
--- samples/HelpBar/FMXHelpBarSample/fMain.pas
+++ samples/HelpBar/FMXHelpBarSample/fMain.pas
@@ -3,11 +3,31 @@ unit fMain;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.Objects,
+  FMX.Controls.Presentation,
+  FMX.StdCtrls,
+  Gamolf.FMX.HelpBar;
 
 type
   TForm2 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    DGEFMXHelpBar1: TDGEFMXHelpBar;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
   private
     { Déclarations privées }
   public
@@ -20,5 +40,65 @@ var
 implementation
 
 {$R *.fmx}
+
+uses
+  Olf.Skia.SVGToBitmap,
+  USVGKenneyInputKeys,
+  USVGKenneyInputXbox;
+
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  DGEFMXHelpBar1.CloseHelpBar;
+  DGEFMXHelpBar1.OpenHelpBar;
+  DGEFMXHelpBar1.HorzAlign := TDGEFMXHelpBarHorzAlign.Center;
+  DGEFMXHelpBar1.VertAlign := TDGEFMXHelpBarVertAlign.Bottom;
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardArrowDown,
+    TSVGKenneyInputXbox.XboxDpadRoundDown, 'down');
+end;
+
+procedure TForm2.Button2Click(Sender: TObject);
+begin
+  DGEFMXHelpBar1.CloseHelpBar;
+  DGEFMXHelpBar1.OpenHelpBar;
+  DGEFMXHelpBar1.HorzAlign := TDGEFMXHelpBarHorzAlign.Left;
+  DGEFMXHelpBar1.VertAlign := TDGEFMXHelpBarVertAlign.Top;
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardArrowDown,
+    TSVGKenneyInputXbox.XboxDpadRoundDown, 'down');
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardEscape,
+    TSVGKenneyInputXbox.XboxButtonColorX, 'quit');
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardSpace,
+    TSVGKenneyInputXbox.XboxButtonColorA, 'go');
+end;
+
+procedure TForm2.Button3Click(Sender: TObject);
+begin
+  DGEFMXHelpBar1.CloseHelpBar;
+  DGEFMXHelpBar1.OpenHelpBar;
+  DGEFMXHelpBar1.HorzAlign := TDGEFMXHelpBarHorzAlign.right;
+  DGEFMXHelpBar1.VertAlign := TDGEFMXHelpBarVertAlign.Top;
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardSpace,
+    TSVGKenneyInputXbox.XboxButtonColorA);
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardEscape,
+    TSVGKenneyInputXbox.XboxButtonColorX, 'quit');
+  DGEFMXHelpBar1.AddItem(TSVGKenneyInputKeys.KeyboardSpace,
+    TSVGKenneyInputXbox.XboxButtonColorA, 'go');
+end;
+
+procedure TForm2.FormCreate(Sender: TObject);
+begin
+  DGEFMXHelpBar1.IconGamepadBitmapListIndex := TOlfSVGBitmapList.AddAList;
+  TOlfSVGBitmapList.AddItem(DGEFMXHelpBar1.IconGamepadBitmapListIndex,
+    SVGKenneyInputXbox);
+
+  DGEFMXHelpBar1.IconKeyBitmapListIndex := TOlfSVGBitmapList.AddAList;
+  TOlfSVGBitmapList.AddItem(DGEFMXHelpBar1.IconKeyBitmapListIndex,
+    SVGKenneyInputKeys);
+end;
+
+initialization
+
+{$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := true;
+{$ENDIF}
 
 end.
