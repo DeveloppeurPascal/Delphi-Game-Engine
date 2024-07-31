0d87e8cbb6591ed866ce6ed885165e58e3ce099b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-31 11:21:41 +0200

    added supported platforms for the components (#178, #179)

--- src/Gamolf.RTL.Joystick.Deprecated.pas
+++ src/Gamolf.RTL.Joystick.Deprecated.pas
@@ -3,6 +3,7 @@ unit Gamolf.RTL.Joystick.Deprecated;
 interface
 
 uses
+  System.Classes,
   Gamolf.RTL.Joystick;
 
 {$MESSAGE WARN 'Unit Gamolf.RTL.Joystick.Deprecated contains only deprecated classes for migration purpose. You should use non deprecated features in your projects.'}
@@ -11,21 +12,36 @@ type
   /// <summary>
   /// DEPRECATED component, use TDGEGamepadManager
   /// </summary>
+{$IF CompilerVersion >= 33.0}
+  [ComponentPlatformsAttribute(pfidWindows or pfidOSX or pfidLinux or
+    pfidAndroid or pfidiOS)]
+{$ELSE}
+  [ComponentPlatformsAttribute(pidWin32 or pidWin64 or pidOSX32 or pidOSX64 or
+    pidiOSSimulator or pidiOSDevice32 or pidiOSDevice64 or pidAndroid or
+    pidAndroid64 or pidLinux64)]
+{$ENDIF}
+
   TGamepadManager = class(TDGEGamepadManager)
   end;
 
   /// <summary>
   /// DEPRECATED component, use TDGEGamepad
   /// </summary>
+{$IF CompilerVersion >= 33.0}
+  [ComponentPlatformsAttribute(pfidWindows or pfidOSX or pfidLinux or
+    pfidAndroid or pfidiOS)]
+{$ELSE}
+  [ComponentPlatformsAttribute(pidWin32 or pidWin64 or pidOSX32 or pidOSX64 or
+    pidiOSSimulator or pidiOSDevice32 or pidiOSDevice64 or pidAndroid or
+    pidAndroid64 or pidLinux64)]
+{$ENDIF}
+
   TGamepad = class(TDGEGamepad)
   end;
 
 procedure Register;
 
 implementation
-
-uses
-  System.Classes;
 
 procedure Register;
 begin
